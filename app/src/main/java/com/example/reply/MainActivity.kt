d1b4f9f8404b01adb26694e069b41ae9682f0547
gaston5050 <80988908+gaston5050@users.noreply.github.com>
2026-01-12 16:25:20 -0300

    Segundo commit

--- app/src/main/java/com/example/reply/MainActivity.kt
+++ app/src/main/java/com/example/reply/MainActivity.kt
@@ -26,6 +26,10 @@ import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.material3.Surface
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLayoutDirection
@@ -35,6 +39,7 @@ import com.example.reply.ui.theme.ReplyTheme
 
 class MainActivity : ComponentActivity() {
 
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
@@ -50,19 +55,21 @@ class MainActivity : ComponentActivity() {
                                 .calculateEndPadding(layoutDirection)
                         )
                 ) {
-                    ReplyApp()
+                    val windowSize = calculateWindowSizeClass(this)
+                    ReplyApp(windowSize)
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
 @Preview(showBackground = true)
 @Composable
 fun ReplyAppCompactPreview() {
     ReplyTheme {
         Surface {
-            ReplyApp()
+           ReplyApp(windowSize = WindowWidthSizeClass.Compact,)
         }
     }
 }
--- app/src/main/java/com/example/reply/ui/ReplyApp.kt
+++ app/src/main/java/com/example/reply/ui/ReplyApp.kt
@@ -15,6 +15,7 @@
  */
 package com.example.reply.ui
 
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
@@ -24,10 +25,17 @@ import com.example.reply.data.MailboxType
 
 @Composable
 fun ReplyApp(
+    windowSize: WindowSizeClass,
     modifier: Modifier = Modifier,
 ) {
     val viewModel: ReplyViewModel = viewModel()
     val replyUiState = viewModel.uiState.collectAsState().value
+    when(windowSize){
+        WindowSizeClass.Compact ->{}
+        WindowSizeClass.Medium ->{}
+        WindowSizeClass.Expanded ->{}
+        else ->{}
+    }
 
     ReplyHomeScreen(
         replyUiState = replyUiState,
